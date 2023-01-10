19175db9e23c5594d5e18e553d66a5a54310bece
elvintaghizade14 <elvintaghiyev184@gmail.com>
2023-01-10 12:31:34 +0400

    lesson17 lesson code attached

---
+++ src/main/java/az/atlacademy/se/lesson17/ComplexityAnalysisApp.java
@@ -0,0 +1,27 @@
+package az.atlacademy.se.lesson17;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ComplexityAnalysisApp {
+
+    public static void main(String[] args) {
+        int[] nums = new int[50];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = (int) (Math.random() * 100 + 1);
+        }
+
+        System.out.println(Arrays.toString(nums));
+
+        int numToFind = new Scanner(System.in).nextInt();
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            ++count;
+            if (nums[i] == numToFind) {
+                System.out.println("Index of " + numToFind + " is: " + i);
+            }
+        }
+        System.out.println("Operation count: " + count);
+    }
+
+}
