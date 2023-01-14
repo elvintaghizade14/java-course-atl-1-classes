c485770b0f75a8d0108fa2d7b8b70adae3f35228
elvintaghizade14 <elvintaghiyev184@gmail.com>
2023-01-14 13:21:22 +0400

    lesson16 class code attached

---
+++ src/main/java/az/atlacademy/se/lesson16/FunctionApp.java
@@ -0,0 +1,36 @@
+package az.atlacademy.se.lesson16;
+
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class FunctionApp {
+
+    public static void main(String[] args) {
+        System.out.print("Enter input: ");
+        final String line = new Scanner(System.in).nextLine();
+
+        final String result = new MyFunction().apply(line);
+
+        String result2 = new Function<String, String>() {
+            @Override
+            public String apply(String s) {
+                return s.concat(s);
+            }
+        }.apply(line);
+
+        String result3 = ((Function<String, String>) s -> s.concat(s)).apply(line);
+        System.out.println(result);
+        System.out.println(result2);
+        System.out.println(result3);
+    }
+
+    static class MyFunction implements Function<String, String> {
+
+        @Override
+        public String apply(String s) {
+            return s.concat(s);
+        }
+
+    }
+
+}
